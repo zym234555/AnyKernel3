21e7b143500170a17a1f538095de0640e5006221
Ylarod <me@ylarod.cn>
2023-01-30 22:58:45 +0800

    format

--- anykernel.sh
+++ anykernel.sh
@@ -17,39 +17,38 @@ supported.patchlevels=
 ### AnyKernel install
 # begin attributes
 attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+    set_perm_recursive 0 0 755 644 $ramdisk/*
+    set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin
 } # end attributes
 
 kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
 
 case "$kernel_version" in
-    5.1*) ksu_supported=true;;
-    *) ksu_supported=false;;
+5.1*) ksu_supported=true ;;
+*) ksu_supported=false ;;
 esac
 
-ui_print "  -> ksu_supported: $ksu_supported";
+ui_print "  -> ksu_supported: $ksu_supported"
 
 if [ ! $ksu_supported ]; then
-    ui_print "  -> Non-GKI device, abort.";
-    exit 1;
-fi;
+    ui_print "  -> Non-GKI device, abort."
+    exit 1
+fi
 
 ## boot shell variables
-block=boot;
-is_slot_device=1;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+block=boot
+is_slot_device=1
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh && attributes;
+. tools/ak3-core.sh && attributes
 
 # boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
-
 
 ## init_boot shell variables
 #block=init_boot;
@@ -66,7 +65,6 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 #write_boot;
 ## end init_boot install
 
-
 ## vendor_kernel_boot shell variables
 #block=vendor_kernel_boot;
 #is_slot_device=1;
@@ -82,7 +80,6 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 #flash_boot;
 ## end vendor_kernel_boot install
 
-
 ## vendor_boot shell variables
 #block=vendor_boot;
 #is_slot_device=1;
@@ -97,4 +94,3 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 
 #write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 ## end vendor_boot install
-
