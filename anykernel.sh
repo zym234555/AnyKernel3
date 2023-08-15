63143e22f627c73380e25ef089ee73e144cefdb8
Chris Renshaw <osm0sis@outlook.com>
2023-08-15 20:12:25 -0300

    Update anykernel.sh
    - fix kernel.string display
    - latest template changes
    - a slight tidy

--- anykernel.sh
+++ anykernel.sh
@@ -2,48 +2,44 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# begin properties
-properties() {'kernel.string=KernelSU by KernelSU Developers
+# global properties
+properties() { '
+kernel.string=KernelSU by KernelSU Developers
 do.devicecheck=0
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-'} # end properties
+'; } # end properties
+
 
 ### AnyKernel install
-
 ## boot shell variables
 block=boot
 is_slot_device=auto
 ramdisk_compression=auto
 patch_vbmeta_flag=auto
+no_magisk_check=1
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
 
 kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
-case "$kernel_version" in
-5.1*) ksu_supported=true ;;
-6.1*) ksu_supported=true ;;
-*) ksu_supported=false ;;
+case $kernel_version in
+    5.1*) ksu_supported=true ;;
+    6.1*) ksu_supported=true ;;
+    *) ksu_supported=false ;;
 esac
 
 ui_print " " "  -> ksu_supported: $ksu_supported"
-
-if [ ! $ksu_supported ]; then
-    ui_print " " "  -> Non-GKI device, abort."
-    exit 1
-fi
+$ksu_supported || abort "  -> Non-GKI device, abort."
 
 # boot install
-if [ -L "/dev/block/bootdevice/by-name/init_boot_a" ]; then
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
     split_boot # for devices with init_boot ramdisk
-
     flash_boot # for devices with init_boot ramdisk
 else
     dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
     write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 fi
 ## end boot install
